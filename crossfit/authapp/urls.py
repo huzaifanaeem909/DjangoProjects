ca8fbdaef8bfabc64964362c5d06e73e81122f82
Huzaifa <huzaifanaeem909@gmail.com>
2024-09-24 20:13:35 +0500

    Created Sign Up Page

--- crossfit/authapp/urls.py
+++ crossfit/authapp/urls.py
@@ -3,4 +3,5 @@ from authapp import views
 
 urlpatterns = [
     path('', views.Home, name="Home"),
+    path('signup', views.signup, name="signup"),
 ]
--- crossfit/authapp/views.py
+++ crossfit/authapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def Home(request):
     return render(request, 'index.html')
+
+def signup(request):
+    return render(request, 'signup.html')
